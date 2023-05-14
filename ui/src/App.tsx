6a089967d6ba45a0e2e5bf1a4b72a0f2c717333f
Chris K <Chris-SourceCode@outlook.com>
2023-05-15 00:01:47 +0200

    feat: Add logo to Topmast and style using Material-UI
     - Added Topmast logo using the provided SVG file
     - Improved the visual appearance of the logo in the header
    
    This commit enhances the branding and visual appeal of the Topmast application by adding a logo and applying a custom style.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,11 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 import DashboardView from './views/DashboardView';
 import ContainerView from './views/ContainerView';
 import LogsDashboard from './components/LogsDashboard';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Logo from '../../topmast_cruise.svg';
 
 export function App() {
   return (
     <>
-      <h2>Topmast</h2>
+      <Box display="flex" alignItems="center">
+        <Typography variant="h2">Topmast</Typography>
+        <img
+          src={Logo}
+          alt="Topmast Logo"
+          style={{ marginLeft: '10px', width: '50px', height: '50px' }}
+        />
+      </Box>
 
       <Routes>
         <Route path="/" element={<DashboardView />} />
