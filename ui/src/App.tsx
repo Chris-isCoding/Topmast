f4018d86e065f6968b486a13083e44320fd7f1c9
Chris K <Chris-SourceCode@outlook.com>
2023-05-16 22:36:35 +0200

    feat: Add navigation and style enhancements to component
     - Added Button and ButtonGroup components from @mui/material
     - Styled the ButtonGroup component with margin-bottom
     - Placed the navigation buttons within the ButtonGroup component
     - Imported Typography, Toolbar, and Box components from @mui/material
     - Updated the layout and styling of the component to include Topmast branding
     - Positioned the logo next to the Topmast text
     - Applied spacing and alignment adjustments to improve the overall style
     - Updated the component to use react-router-dom's Link for navigation
    
    This commit introduces navigation functionality and style enhancements to the component, improving its overall usability and visual appeal.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,22 +1,38 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import DashboardView from './views/DashboardView';
 import ContainerView from './views/ContainerView';
 import LogsDashboard from './components/LogsDashboard';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Typography, Button, ButtonGroup, Box } from '@mui/material';
+
 import Logo from '../assets/topmast_cruise.svg';
 
 export function App() {
   return (
     <>
-      <Box display="flex" alignItems="center">
-        <Typography variant="h2">Topmast</Typography>
+      <Box sx={{ marginBottom: '20px' }} display="flex" alignItems="center">
+        <Typography variant="h2" sx={{ marginRight: '10px' }}>
+          Topmast
+        </Typography>
         <img
           src={Logo}
           alt="Topmast Logo"
-          style={{ marginLeft: '10px', width: '50px', height: '50px' }}
+          style={{ width: '50px', height: '50px' }}
         />
       </Box>
+
+      <ButtonGroup
+        sx={{ marginBottom: '20px' }}
+        size="small"
+        variant="contained"
+        aria-label="main navigation"
+      >
+        <Button component={Link} to="/">
+          Dashboard
+        </Button>
+        <Button component={Link} to="/containerlogs">
+          Logs
+        </Button>
+      </ButtonGroup>
 
       <Routes>
         <Route path="/" element={<DashboardView />} />
